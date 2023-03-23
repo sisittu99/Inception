5eb81fa07d60e3a931cfd487e999a3d31cfe2313
sisittu99 <sisittu99@hotmail.it>
2023-03-23 16:23:32 +0100

    Da modificare - inizio debug wordpress/config.sh 23/03

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -2,7 +2,7 @@
 
 #wait for mariadb, then connect with credentials
 
-while ! mariadb -h $MYSQL_HOSTNAME -u $WP_DB_USER -p $WP_DB_PASSWORD $WP_DB_NAME &>/dev/null;
+while ! mariadb -h $DATABASE_NAME -u $DATABASE_ADMIN -p $DATABASE_ADMIN_PWD $DATABASE_NAME &>/dev/null;
 do
     sleep 3
 done
@@ -14,7 +14,7 @@ then
 
 
 	wp core download --allow-root
-	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_ADMIN --dbpass=$DATABASE_ADMIN_PWD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp db create --allow-root
 	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
