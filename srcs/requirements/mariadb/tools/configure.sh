2c319e34b04ab918bccab4f3a9326a4e54f0e2dc
alazzari <alazzari@alazzari.myguest.virtualbox.org>
2023-03-24 20:25:57 +0100

    Added my.cnf, trying to fix mariaDB (user/pwd still not working) 24/03

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -27,13 +27,16 @@ DROP DATABASE test; #cancella la tabella test
 DELETE FROM mysql.db WHERE Db='test'; #cancella dalla tabella db il database test
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); #cancella dalla tabella degli user il campo root quando l'indirizzo di provenienza non è il localhost
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD'; #cambia password utente root da localhost
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; #cambia password utente root da localhost
 
-CREATE DATABASE $DATABASE_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE USER $DATABASE_ADMIN'@'%' IDENTIFIED BY '$DATABASE_ADMIN_PWD';
-GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_ADMIN'@'%';
+CREATE DATABASE $WP_DB_NAME DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE USER $WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
+
+# IF NOT EXISTS $WP_DB_NAME
+# IF NOT EXISTS $WP_DB_ADMIN IDENTIFIED BY $WP_DB_PASSWORD
 
     /usr/bin/mysqld --user=mysql --bootstrap < $tmp
     rm -f $tmp
--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "mariadb $(getent hosts mariadb | awk '{ print $1 }')" >> /etc/hosts
+
 #wait for mariadb, then connect with credentials
 
 ##
-while ! mysql --host=$WP_DB_HOST --user=$WP_DB_USER --password=$WP_DB_PASSWORD $WP_DB_NAME &>/dev/null;
+while ! mysql -h$MYSQL_HOSTNAME -u$WP_DB_USER -p$WP_DB_PASSWORD $WP_DB_NAME &>/dev/null;
 do
     sleep 3
 done
@@ -18,7 +20,7 @@ then
 
 	## variabili
 	wp core download --allow-root
-	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=$WP_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+	wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWORD --dbhost=$MYSQL_HOSTNAME --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
 	wp db create --allow-root
 	wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
